a3026dcbd42d9cbc96ab39aae2a0032619b5484e
Premdev05 <premkumar200305@gmail.com>
2025-12-09 10:27:07 +0530

    Starting....

---
+++ README.md
@@ -0,0 +1,31 @@
+# Portfolio
+
+A modern developer-focused portfolio template built with **Vite**, **React**, and **Tailwind CSS**. This template is fully customizable, responsive, and integrates **Web3Forms** for seamless form submission. Ideal for showcasing your projects, skills, and experience.
+
+
+## Features
+
+- **Fast & Modern:** Built with Vite for optimized performance.
+- **Responsive Design:** Works seamlessly on all devices.
+- **Customizable:** Easily adapt the template to fit your needs.
+- **Form Integration:** Web3Forms for simple and secure form submissions.
+- **Tailwind CSS:** Stylish and efficient utility-first CSS framework.
+
+
+### Prerequisites
+
+- Node.js installed on your system.
+- Git installed.
+
+## Usage
+
+- Customize the content and styles in the `src` directory to fit your personal or professional needs.
+- Configure Web3Forms in the designated form component for form submissions.
+
+## Contributing
+
+Contributions are welcome! Feel free to fork the repository and submit a pull request.
+
+---
+
+Happy Coding! 🎉
---
+++ src/components/Home.jsx
@@ -0,0 +1,69 @@
+import Typewriter from "typewriter-effect";
+import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+
+const Home = () => {
+  return (
+    <div id="home" className="bg-[#FFFFFF]">
+      <div className="max-w-[85%] mx-auto font-inter grid grid-cols-1 md:grid-cols-2 gap-8 items-center  min-h-screen">
+        {/* Left Section */}
+        <div className="order-2 lg:order-1 flex flex-col items-center md:items-start justify-start space-y-5 text-center md:text-left">
+          <h1 className="text-3xl md:text-5xl font-bold">
+            I’m <span className="text-[#fe5617]">Premkumar</span>
+          </h1>
+          <h2 className="text-2xl md:text-3xl font-bold flex gap-2">
+            <span className="text-[#fe5617]">I’m a</span>
+            <Typewriter
+              options={{
+                strings: [
+                  "Developer.",
+                  "Designer."
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 50,
+                deleteSpeed: 30,
+              }}
+            />
+          </h2>
+          {/* About Me */}
+          <p className="text-lg md:text-xl text-gray-700 font-medium">
+            A entry level front end developer,
+            dedicated to building user-friendly web applications.
+          </p>
+
+          {/* Contact Information */}
+          <div className=" flex flex-col space-y-3 md:space-y-0 md:flex-row md:gap-6 text-lg md:text-xl font-semibold ">
+            <div className="flex items-center gap-3">
+              <FaLocationDot className="text-[#fe5617]" size={20} />
+              <span>Tirupur</span>
+            </div>
+            <div className="flex items-center gap-3">
+              <FaPhoneAlt className="text-[#fe5617]" size={20} />
+              <span>+91 6369340852</span>
+            </div>
+            <div className="flex items-center gap-3">
+              <FaEnvelope className="text-[#fe5617]" size={20} />
+              <span>premkumar200305@gmail.com</span>
+            </div>
+          </div>
+          {/* Button */}
+          <div className="bg-[#fe5617] px-5 py-2 rounded-2xl font-bold ">
+            <a href="/PREMKUMAR M_Developer.pdf" download>Download CV</a>
+          </div>
+        </div>
+
+        {/* Right Section */}
+        <div className="order-1 justify-center lg:order-2 flex lg:justify-end items-center">
+          <img
+            className="w-[75%]  rounded-[50px]"
+            src="/profile.webp"
+            alt="Profile"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
